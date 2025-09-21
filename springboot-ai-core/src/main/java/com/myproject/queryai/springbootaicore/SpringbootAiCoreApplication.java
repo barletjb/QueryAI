a1330650491bc8be41a62735995e27bcdd6515f9
barletjb <barletjb.perso@gmail.com>
2025-09-21 23:03:39 +0200

    first commit

---
+++ springboot-ai-core/src/main/java/com/myproject/queryai/springbootaicore/SpringbootAiCoreApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.queryai.springbootaicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAiCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAiCoreApplication.class, args);
+    }
+
+}
---
+++ springboot-ai-core/src/test/java/com/myproject/queryai/springbootaicore/SpringbootAiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.queryai.springbootaicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAiCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
